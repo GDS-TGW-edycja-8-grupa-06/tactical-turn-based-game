484d3630a78e06ec81f60a24dc8e93c4cac34945
Bodzio Podolak, Jr <bodzio2k@live.com>
2021-02-16 22:36:34 +0100

    Some bullshit drawing.

---
+++ Assets/_Scripts/Tag.cs
@@ -0,0 +1,7 @@
+﻿public enum Tag
+{
+    Tag01,
+    Tag02,
+    Tag03,
+    Tag04
+}
---
+++ Assets/_Scripts/Unit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "UnitName", menuName = "Unit")]
+public class Unit : ScriptableObject
+{
+    [SerializeField]
+    private int health = 100;
+
+    [SerializeField]
+    private Tag[] tags;
+
+    [SerializeField]
+    private int damageDealt = 0;
+
+    [SerializeField]
+    private Sprite[] sprites;
+}

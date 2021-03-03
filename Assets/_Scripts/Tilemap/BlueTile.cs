f5a01bfac732f60190d9ff203418fbaf24a9a008
Bodzio Podolak, Jr <bodzio2k@live.com>
2021-03-03 19:14:53 +0100

    Dodałem niebieskie kafelki pod postacią trymien.

---
+++ Assets/_Scripts/Tilemap/BlueTile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueTile : MonoBehaviour
+{
+    private Unit unit = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Unit>(out unit))
+        {
+            unit.battleSystem.sm.changeState<BattleSystemEnteringWinnigArea>();
+            unit.sm.changeState<UnitIdle>();
+        };
+    }
+}

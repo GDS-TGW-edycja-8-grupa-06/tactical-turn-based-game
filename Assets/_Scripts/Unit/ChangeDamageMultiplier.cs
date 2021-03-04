5b6940434416e0bf68ce0d518582afcb763c732b
Bodzio Podolak, Jr <bodzio2k@live.com>
2021-03-04 13:35:28 +0100

    Jeszcze jedna klasa do namespace-u.

--- Assets/_Scripts/Unit/ChangeDamageMultiplier.cs
+++ Assets/_Scripts/Unit/ChangeDamageMultiplier.cs
@@ -1,28 +1,31 @@
 ﻿using Prime31.StateKit;
 using Bodzio2k.Unit;
 
-internal class ChangeDamageMultiplier : SKState<Unit>
+namespace Bodzio2k.Unit
 {
-    public override void begin()
+    public class ChangeDamageMultiplier : SKState<Unit>
     {
-        base.begin();
+        public override void begin()
+        {
+            base.begin();
 
-        float damageMultiplier = 0.0f;
+            float damageMultiplier = 0.0f;
 
-        damageMultiplier = _context.unitProperties.damageMultiplier == 1.0f ? 0.5f : 1.0f;
+            damageMultiplier = _context.unitProperties.damageMultiplier == 1.0f ? 0.5f : 1.0f;
 
-        _context.unitProperties.damageMultiplier = damageMultiplier;
+            _context.unitProperties.damageMultiplier = damageMultiplier;
 
-        _context.sm.changeState<Idle>();
-    }
+            _context.sm.changeState<Idle>();
+        }
 
-    public override void end()
-    {
-        base.end();
-    }
+        public override void end()
+        {
+            base.end();
+        }
 
-    public override void update(float deltaTime)
-    {
-        return;
+        public override void update(float deltaTime)
+        {
+            return;
+        }
     }
 }

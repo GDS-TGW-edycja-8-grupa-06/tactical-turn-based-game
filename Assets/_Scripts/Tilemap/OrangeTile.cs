8d9a05516f4135d849416293e59fedef8fa60286
Bodzio Podolak, Jr <bodzio2k@live.com>
2021-03-26 12:32:53 +0100

    Dopisanie do namespace.

--- Assets/_Scripts/Tilemap/OrangeTile.cs
+++ Assets/_Scripts/Tilemap/OrangeTile.cs
@@ -2,23 +2,26 @@
 using Bodzio2k.BattleSystem;
 using Bodzio2k.Unit;
 
-public class OrangeTile : MonoBehaviour
+namespace Bodzio2k
 {
-    private Unit unit = null;
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    public class OrangeTile : MonoBehaviour
     {
-        if (collision.gameObject.TryGetComponent<Unit>(out unit))
-        {
-            unit.sm.changeState<EnteringWinnigArea>();
-        };
-    }
+        private Unit.Unit unit = null;
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.TryGetComponent<Unit>(out unit))
+        private void OnTriggerEnter2D(Collider2D collision)
         {
-            unit.sm.changeState<LeavingWinnigArea>();
-        };
+            if (collision.gameObject.TryGetComponent<Unit.Unit>(out unit))
+            {
+                unit.sm.changeState<EnteringWinnigArea>();
+            };
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.TryGetComponent<Unit.Unit>(out unit))
+            {
+                unit.sm.changeState<LeavingWinnigArea>();
+            };
+        }
     }
 }
--- Assets/_Scripts/Tilemap/WhiteTile.cs
+++ Assets/_Scripts/Tilemap/WhiteTile.cs
@@ -1,27 +1,30 @@
 ﻿using UnityEngine;
 using Bodzio2k.Unit;
 
-public class WhiteTile : MonoBehaviour
+namespace Bodzio2k
 {
-    private Unit unit = null;
-    
-    private void OnTriggerEnter2D(Collider2D collision)
+    public class WhiteTile : MonoBehaviour
     {
-        if (collision.gameObject.TryGetComponent<Unit>(out unit))
+        private Unit.Unit unit = null;
+
+        private void OnTriggerEnter2D(Collider2D collision)
         {
-            Debug.LogFormat("{0} entered reduced damage area...", collision.gameObject.name);
+            if (collision.gameObject.TryGetComponent<Unit.Unit>(out unit))
+            {
+                Debug.LogFormat("{0} entered reduced damage area...", collision.gameObject.name);
 
-            unit.sm.changeState<ChangeDamageMultiplier>();
-        };
-    }
+                unit.sm.changeState<ChangeDamageMultiplier>();
+            };
+        }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.TryGetComponent<Unit>(out unit))
+        private void OnTriggerExit2D(Collider2D collision)
         {
-            Debug.LogFormat("{0} leaved reduced damage area...", collision.gameObject.name);
+            if (collision.gameObject.TryGetComponent<Unit.Unit>(out unit))
+            {
+                Debug.LogFormat("{0} leaved reduced damage area...", collision.gameObject.name);
 
-            unit.sm.changeState<ChangeDamageMultiplier>();
-        };
+                unit.sm.changeState<ChangeDamageMultiplier>();
+            };
+        }
     }
 }
